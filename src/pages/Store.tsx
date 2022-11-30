b502955b51bc28e61d38e0f8d4f87564c0bd1464
bquaiatti <quaiatti@gmail.com>
2022-11-30 18:20:56 +0000

    edit title

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,7 +5,7 @@ import storeItems from "../data/items.json"
 export function Store() {
   return (
     <>
-      <h1>Laptop Store</h1>
+      <h1>Laptop SuperStore</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
           <Col key={item.id}>
